a36072e20d186db0ce4ab91e46d170c9228b0c71
eladruby <60287437+eladruby@users.noreply.github.com>
2025-04-20 22:43:41 +0300

    Update interface.py

--- src/interface.py
+++ src/interface.py
@@ -6,11 +6,8 @@ import tensorflow as tf
 import streamlit as st
 from PIL import Image
 import numpy as np
-import requests
 import os
 
-
-#מודל טריפלט
 class TripletLoss:
     def __init__(self):
         self.inp = tf.keras.Input(shape=(None, None, 3))
@@ -27,7 +24,6 @@ class TripletLoss:
         self.out = tf.keras.layers.GlobalMaxPooling2D()(out_conv)
         return Model(inputs=vgg16_fe.inputs, outputs=self.out, name='embedding')
 
-#פונקציית עיבוד תמונה
 def preprocess(uploaded_file):
     image = Image.open(uploaded_file).convert("RGB")
     w_target = 400
@@ -43,7 +39,6 @@ def preprocess(uploaded_file):
 model_copy = TripletLoss()
 embedding = model_copy.embedding()
 
-#בניית מודל והכנסת משקולות
 embedding.build((None, 75, 400, 3))
 
 weights_path = os.path.join(os.path.dirname(__file__), "..", "models", "final.weights.h5")
@@ -51,17 +46,15 @@ embedding.load_weights(weights_path)
 
 st.title("Iris Authentication System")
 
-img_file1 = st.file_uploader("Upload image 1 jpg file only*", type=["jpg"])
-img_file2 = st.file_uploader("Upload image 2 jpg file only*", type=["jpg"])
+img_file1 = st.file_uploader("Upload Image 1 jpg file only*", type=["jpg"])
+img_file2 = st.file_uploader("Upload Image 2 jpg file only*", type=["jpg"])
 
-#בדיקה אם הועלו שתי תמונות
 if img_file1 and img_file2:
     img1 = preprocess(img_file1)
     img2 = preprocess(img_file2)
 
-    #בדיקה אם נמצאה קשתית בשתי התמונות
     if img1 is None or img2 is None:
-        st.error("Iris is not recognized")
+        st.error("Could not find iris")
     else:
         # תצוגת התמונות
         st.image([img1.squeeze() / 255.0, img2.squeeze() / 255.0],
@@ -72,7 +65,7 @@ if img_file1 and img_file2:
         embedding2 = embedding.predict(img2)
         distance = norm(embedding1 - embedding2)
 
-        st.write("**Distance between embeddings:**", float(distance))
+        st.write("🔍 **Distance between embeddings:**", float(distance))
         threshold = 1.49
         if distance < threshold:
             st.success("Authentication Successful")
